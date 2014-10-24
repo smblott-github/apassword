aac77fcb7c310a1fdb935e22f1bf7ec746fa86db
Stephen Blott <stephen@liftoff.io>
2014-10-24 14:54:30 +0100

    Initial Android project.

---
+++ src/org/smblott/apassword/AGPM.java
@@ -0,0 +1,15 @@
+package org.smblott.apassword;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AGPM extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
